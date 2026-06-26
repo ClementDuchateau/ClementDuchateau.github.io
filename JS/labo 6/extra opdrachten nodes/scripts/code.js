a7a21b29e355bce5d56fa38a9412dc605630ffd3
ClementDuchateau <r1052392@student.vives.be>
2026-06-26 22:08:36 +0200

    restart

--- JS/labo 6/extra opdrachten nodes/scripts/code.js
+++
@@ -1,9 +0,0 @@
-const setup = () => {
-    let p = document.querySelectorAll('p')[0];
-    let y =p.childNodes[0];
-    y.remove();
-    let z = document.createTextNode("Good Job!");
-    p.appendChild(z);
-
-}
-window.addEventListener("load", setup);
--- JS/labo2/oef substring/scripts/code.js
+++
@@ -1,9 +0,0 @@
-const setup = () => {
-    let sendButton = document.getElementById('SendButton');
-    sendButton.addEventListener("click", knip);
-}
-const knip = () => {
-    let txtInput = document.getElementById('txtInput');
-    txtOutput.innerHTML = txtInput.value.substring(beginNr.value, amountOfNrs.value);
-}
-window.addEventListener("load", setup);
